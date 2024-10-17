4e3be7048767482cc906342b00756a2cccaa8d78
Jadin Naicker <jadinnaicker1@gmail.com>
2024-10-17 19:08:13 +0200

    Commit 1: LoginViewModel
     I created a Model called LoginViewModels where i made variables for the users login such as their email address and password which users would need to login depending on the type of role they have

---
+++ AfriclaimMVCST10275486/AfriclaimMVCST10275486/Models/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AfriclaimMVCST10275486.Models
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; } //Holds Users email address 
+        public string Password { get; set; } //Holds Users password
+    }
+}
